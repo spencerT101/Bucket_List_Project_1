86863a82d1703eae3fbdf956978138492d87579d
Spencer <Spencer.Taber@yahoo.com>
2021-04-23 16:53:44 +0100

    configured run_test and app.py files

---
+++ project_start_code/run_test.py
@@ -0,0 +1,10 @@
+import unittest
+#from test_modules.test_game import TestGame
+#from test_modules.test_player import TestPlayer
+
+
+if __name__== "__main__":
+    unittest.main()
+
+
+app = Flask(__name__)
